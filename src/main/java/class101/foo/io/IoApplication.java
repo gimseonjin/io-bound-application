71659884c68f8bc1871999f81b2e0bc00059d3d1
gimseonjin <kimseonjin616@naver.com>
2021-03-03 10:41:39 +0900

    test 1

--- src/main/java/class101/foo/io/IoApplication.java
+++ src/main/java/class101/foo/io/IoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class IoApplication {
-
+	//test 1
 	public static void main(String[] args) {
 		SpringApplication.run(IoApplication.class, args);
 	}
